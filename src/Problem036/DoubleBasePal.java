40e356c10a4ec5ab8e8ea5429cc0be5db2302ca6
Yunxin Zhang <yx-z@outlook.com>
2016-01-10 10:36:08 +0800

    34 completed

---
+++ src/Problem036/DoubleBasePal.java
@@ -0,0 +1,23 @@
+package Problem036;
+
+/**
+ * The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+ * <p>
+ * Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+ * <p>
+ * (Please note that the palindromic number, in either base, may not include leading zeros.)
+ */
+
+public class DoubleBasePal {
+    public static void main(String[] args) {
+        
+    }
+
+    private static boolean isPal(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        for (; left < right; left++, right--)
+            if (s.charAt(left) != s.charAt(right)) return false;
+        return true;
+    }
+}
