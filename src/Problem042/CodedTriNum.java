8aecd44113d3d8c9a9dcb19d1ae1025c08a11047
Yunxin Zhang <yx-z@outlook.com>
2016-01-12 20:19:56 +0800

    37 completed

--- src/Problem042/CodedTriNum.java
+++ src/Problem042/CodedTriNum.java
@@ -11,7 +11,8 @@ import java.io.InputStreamReader;
  * 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
  * <p>
  * By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value.
- * For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a triangle number then we shall call the word a triangle word.
+ * For example, the word value for SKY is 19 + 11 + 25 = 55 = t10.
+ * If the word value is a triangle number then we shall call the word a triangle word.
  * <p>
  * Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words.
  * How many are triangle words?
