42da4edf7ae2a28dc22884c0cfc81703d28c8178
Yunxin Zhang <yx-z@outlook.com>
2016-01-31 17:15:05 -0600

    47 completed

--- src/Problem047/DistinctPrimesFact.java
+++ src/Problem047/DistinctPrimesFact.java
@@ -1,5 +1,8 @@
 package Problem047;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * The first two consecutive numbers to have two distinct prime factors are:
  * <p>
@@ -17,6 +20,29 @@ package Problem047;
 
 public class DistinctPrimesFact {
     public static void main(String[] args) {
+        int n = 2 * 3 * 5 * 7;
+        while (!fourConsecutive(n)) n++;
+        System.out.println(n);
+    }
 
+    private static boolean fourConsecutive(int n) {
+        return countPrimeFactors(n) == 4 && countPrimeFactors(n + 1) == 4 && countPrimeFactors(n + 2) == 4 && countPrimeFactors(n + 3) == 4;
+    }
+
+    private static int countPrimeFactors(int n) {
+        int count = 0;
+        for (int i = 2, end = (int) Math.sqrt(n); i <= end; i++) {
+            if (n % i == 0) {
+                //decrease the range
+                do n /= i;
+                while (n % i == 0);
+                count++;
+                //update ending condition
+                end = (int) Math.sqrt(n);
+            }
+        }
+        //if remainder is a prime, add 1
+        if (n > 1) count++;
+        return count;
     }
 }
